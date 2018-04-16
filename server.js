af3b2247d40f27ca1666d4684f106c2e55233364
orlando <orlandoesteban@hotmail.cl>
2018-04-16 13:01:57 -0300

    carga proyecto

--- server.js
+++ server.js
@@ -1,20 +1,44 @@
 var express = require('express');
 var app = express();
 
+var hbs = require('hbs');
 //midlewares que devuelve paginas html
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerPartials(__dirname + '/views/parciales');
+
 app.set('view engine', 'hbs');
+
+
+//puerto para heroku
+const port = process.env.PORT || 3000;
+
+//helpers
+hbs.registerHelper('getAnio', () => {
+    return new Date().getFullYear();
+});
+
+
 
 app.get('/', (req, res) => {
 
     res.render('home', {
-        nombre: 'orlando',
-        anio: new Date().getFullYear()
+        nombre: 'orlando'
     });
 });
 
+app.get('/home', (req, res) => {
 
-app.listen(3000, () => {
-    console.log('escuchando peticiones puerto 3000');
+    res.render('home', {
+        nombre: 'orlando'
+    });
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about', {});
+});
+
+app.listen(port, () => {
+    console.log('escuchando peticiones puerto ' + port);
 });
